abcb7ddd0160ffc026cfa7ea909fe552fead6b4a
tiago camargo de oliveira <tiago.olive.cam@hotmail.com>
2021-05-11 15:40:28 -0300

    Repositório

---
+++ Infrastructure/Configuration/ContextBase.cs
@@ -0,0 +1,36 @@
+﻿using Entities.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Configuration
+{
+    public class ContextBase : DbContext
+    {
+        public ContextBase(DbContextOptions<ContextBase> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Produto> Produto { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(GetStringConectionConfig());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        private string GetStringConectionConfig()
+        {
+            string strCon = "Data Source=DESKTOP-AIEAQ62; Database=DDD_ECOMMERCE; Trusted_Connection=true";
+            return strCon;
+        }
+
+    }
+}
